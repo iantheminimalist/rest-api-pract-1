18563e561a0c82c4c7598394f27209d3febce0bb
Ian De Jesus <ian.dejesus.fontilea@gmail.com>
2024-04-21 01:22:09 -0700

    testing push for backend stuff

---
+++ api/models/Todo.js
@@ -0,0 +1,23 @@
+const { text } = require('express');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const todoSchema = new Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    timestamp: {
+        type: String,
+        default: Date.now()
+    }
+});
+
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+module.exports = Todo;
